38db5bc537f7c17d3a62a2d63c668932bfe0c472
hazelisles <zhccj19@gmail.com>
2021-05-03 15:59:48 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express()
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
-const Todo = require('./models/todo')
+const routes = require('./routes')
+
 
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -22,63 +23,8 @@ app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(routes)
 
-app.get('/', (req, res) => {
-  // get all data of todo
-  Todo.find()
-    .lean()
-    .sort({_id: 'asc'})
-    .then(todos => res.render('index', { todos }))
-    .catch(error => console.log(error))
-})
-
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
-
-app.post('/todos/', (req, res) => {
-  const newTodo = req.body.name
-  return Todo.create({ name: newTodo })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-app.get('/todos/:id', (req, res) => {
-  const { id } = req.params
-  return Todo.findById(id)
-    .lean()
-    .then(todo => res.render('detail', { todo }))
-    .catch(error => console.log(error))
-})
-
-app.get('/todos/:id/edit', (req, res) => {
-  const { id } = req.params
-  return Todo.findById(id)
-    .lean()
-    .then(todo => res.render('edit', { todo }))
-    .catch(error => console.log(error))
-})
-
-app.put('/todos/:id', (req, res) => {
-  const { id } = req.params
-  const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === "on"
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
-
-app.delete('/todos/:id', (req, res) => {
-  const { id } = req.params
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
 
 app.listen(3000, () => {
   console.log('APP is now running on http://localhost:3000')
