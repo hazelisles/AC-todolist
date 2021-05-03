c60e76f824c1e928d8589934d2f16a67e5a64a0a
hazelisles <zhccj19@gmail.com>
2021-05-04 02:19:56 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+
 const routes = require('./routes')
-
-
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -24,7 +12,6 @@ app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
-
 
 app.listen(3000, () => {
   console.log('APP is now running on http://localhost:3000')
